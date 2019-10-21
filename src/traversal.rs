380fc7021adae345f4f6139f73e3d11926beaafa
Chuck Rai <cprai@sfu.ca>
2019-10-20 20:05:58 -0700

    Going to try using boxes now

--- src/traversal.rs
+++ src/traversal.rs
@@ -1,41 +1,58 @@
 use rustpython_parser::{ast, parser, location};
 
-enum ASTNode {
-    Some(ast::Expression),
-    Some(ast::Statement),
-    Some(ast::Suite),
+trait ASTNode {
+    fn children(&self) -> Vec<Box<dyn ASTNode>>;
 }
 
-fn expression_children(expression: ast::Expression) -> Vec<ASTNode> {
-    match expression.node {
-        ast::ExpressionType::Identifier {name} => Vec::new(),
-        ast::ExpressionType::Number {value} => Vec::new(),
-        ast::ExpressionType::BoolOp {op, values} => values,
-        ast::ExpressionType::Binop {a, op, b} => vec![*a, *b],
-        ast::ExpressionType::Unop {op, a} => vec![*a],
-        ast::ExpressionType::Compare {vals, ops} => vals,
-        ast::ExpressionType::True => Vec::new(),
-        ast::ExpressionType::False => Vec::new(),
-        ast::ExpressionType::None => Vec::new(),
+impl ASTNode for ast::Expression {
+    fn children(&self) -> Vec<dyn ASTNode> {
+        match &self.node {
+            ast::ExpressionType::Identifier {name} => Vec::new(),
+            ast::ExpressionType::Number {value} => Vec::new(),
+            ast::ExpressionType::BoolOp {op, values} => {
+                let ret: Vec<&dyn ASTNode> = Vec::new();
+                for expression in values {
+                    ret.push(&expression);
+                }
+                ret
+            },
+            ast::ExpressionType::Binop {a, op, b} => vec![&*a, &*b],
+            ast::ExpressionType::Unop {op, a} => vec![&*a],
+            ast::ExpressionType::Compare {vals, ops} => {
+                let ret: Vec<&dyn ASTNode> = Vec::new();
+                for expression in vals {
+                    ret.push(&expression);
+                }
+                ret
+            },
+            ast::ExpressionType::True => Vec::new(),
+            ast::ExpressionType::False => Vec::new(),
+            ast::ExpressionType::None => Vec::new(),
 
-        _ => unreachable!(),
+            _ => unreachable!(),
+        }
     }
 }
 
-fn statement_children(statement: ast::Statement) -> Vec<ASTNode> {
-    match statement.node {
-        ast::StatementType::Break => Vec::new(),
-        ast::StatementType::Continue => Vec::new(),
-        ast::StatementType::Pass => Vec::new(),
-        ast::StatementType::Assign {targets, value} => {targets.push(value); targets},
-        ast::StatementType::Expression {expression} => vec![expression],
-        //ast::StatementType::If {test, body, orelse} => 
-        //ast::StatementType::While {test, body, orelse} => {
+impl ASTNode for &ast::Statement {
+    fn children(&self) -> Vec<&dyn ASTNode> {
+        match &self.node {
+            ast::StatementType::Break => Vec::new(),
+            ast::StatementType::Continue => Vec::new(),
+            ast::StatementType::Pass => Vec::new(),
+            ast::StatementType::Assign {targets, value} => {
+                let ret: Vec<&dyn ASTNode> = Vec::new();
+                for expression in targets {
+                    ret.push(&expression);
+                }
+                ret.push(&value);
+                ret
+            },
+            ast::StatementType::Expression {expression} => vec![&expression],
+            //ast::StatementType::If {test, body, orelse} => 
+            //ast::StatementType::While {test, body, orelse} => {
 
-        _ => unreachable!(),
+            _ => unreachable!(),
+        }
     }
-}
-
-fn suite_children(suite: ast::Suite) -> Vec<ASTNode> {
-    return suite;
 }
