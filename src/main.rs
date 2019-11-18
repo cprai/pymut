dbdd481e55d624ab3301c15cc71b49d8f7899e8b
Chuck Rai <cprai@sfu.ca>
2019-11-18 15:27:55 -0800

    Callback based traversal visitor

--- src/main.rs
+++ src/main.rs
@@ -18,28 +18,33 @@ fn mutate(ast: ast::Program, location: location::Location) -> ast::Program {
     return ast;
 }
 
-fn id_from_location(prefix: &str, location: &location::Location) -> String {
-    format!("_{}_line{}_{}", prefix, location.row(), location.column())
+fn take(expr: &mut ast::Expression) {
+    println!("{} {}", expr.location.row(), expr.location.column());
 }
 
 fn main() {
-    let file = fs::read_to_string("test.py").expect("");
+    let file = fs::read_to_string("django.py").expect("");
     //let program: ast::Program = mutate(parser::parse_program(&file).unwrap(), ast::Location::new(20, 5));
     let mut program: ast::Program = parser::parse_program(&file).unwrap();
     //let program2: ast::Program = program;
     //let program3: ast::Program = program2;
 
-    program.statements.visit(&ast::Location::new(24, 11));
-    match compile::compile_program(program, "".to_string(), 0) {
-        Ok(code_object) => {
-            let settings = PySettings::default();
-            let vm = VirtualMachine::new(settings);
-            let scope = vm.new_scope_with_builtins();
-            //let code = obj::objcode::PyCode::new(code_object);
-            //let codeRef = PyRef{code};
-            let context = pyobject::PyContext::default();
-            vm.run_code_obj(context.new_code_object(code_object), scope);
-        },
-        Err(error) => unreachable!(),
-    }
+    //program.visit(&ast::Location::new(319, 30));
+
+    //program.visit(&|expr: ast::Expression| println!("{} {}", expr.location.row, expr.location.column));
+    program.visit(&take);
+
+
+    //match compile::compile_program(program, "".to_string(), 0) {
+    //    Ok(code_object) => {
+    //        let settings = PySettings::default();
+    //        let vm = VirtualMachine::new(settings);
+    //        let scope = vm.new_scope_with_builtins();
+    //        //let code = obj::objcode::PyCode::new(code_object);
+    //        //let codeRef = PyRef{code};
+    //        let context = pyobject::PyContext::default();
+    //        vm.run_code_obj(context.new_code_object(code_object), scope);
+    //    },
+    //    Err(error) => unreachable!(),
+    //}
 }
--- src/traversal.rs
+++ src/traversal.rs
@@ -1,148 +1,141 @@
-use rustpython_parser::{ast, location};
+use rustpython_parser::ast;
 
 pub trait Visitor {
-    fn visit(&mut self, location: &location::Location);
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>));
 }
 
 impl Visitor for ast::Program {
-    fn visit(&mut self, location: &location::Location) {
-        self.statements.visit(location);
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
+        self.statements.visit(callback);
     }
 }
 
 impl<T: Visitor> Visitor for Option<T> {
-    fn visit(&mut self, location: &location::Location) {
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
         match self {
-            Some(visitable) => visitable.visit(location),
+            Some(visitable) => visitable.visit(callback),
             None => (),
         }
     }
 }
 
 impl<T: Visitor> Visitor for Vec<T> {
-    fn visit(&mut self, location: &location::Location) {
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
         for visitable in self {
-            visitable.visit(location);
+            visitable.visit(callback);
         }
     }
 }
 
 impl<T: Visitor> Visitor for Box<T> {
-    fn visit(&mut self, location: &location::Location) {
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
         let visitable: &mut T = &mut *self;
-        visitable.visit(location);
+        visitable.visit(callback);
     }
 }
 
 impl<T1: Visitor, T2: Visitor> Visitor for (T1, T2) {
-    fn visit(&mut self, location: &location::Location) {
-        self.0.visit(location);
-        self.1.visit(location);
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
+        self.0.visit(callback);
+        self.1.visit(callback);
     }
 }
 
 impl Visitor for ast::Comprehension {
-    fn visit(&mut self, location: &location::Location) {
-        self.target.visit(location);
-        self.iter.visit(location);
-        self.ifs.visit(location);
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
+        self.target.visit(callback);
+        self.iter.visit(callback);
+        self.ifs.visit(callback);
     }
 }
 
 impl Visitor for ast::ComprehensionKind {
-    fn visit(&mut self, location: &location::Location) {
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
         match self {
-            ast::ComprehensionKind::GeneratorExpression {element} => element.visit(location),
-            ast::ComprehensionKind::List {element} => element.visit(location),
-            ast::ComprehensionKind::Set {element} => element.visit(location),
-            ast::ComprehensionKind::Dict {key, value} => { key.visit(location); value.visit(location); },
+            ast::ComprehensionKind::GeneratorExpression {element} => element.visit(callback),
+            ast::ComprehensionKind::List {element} => element.visit(callback),
+            ast::ComprehensionKind::Set {element} => element.visit(callback),
+            ast::ComprehensionKind::Dict {key, value} => { key.visit(callback); value.visit(callback); },
         }
     }
 }
 
 impl Visitor for ast::Keyword {
-    fn visit(&mut self, location: &location::Location) {
-        self.value.visit(location);
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
+        self.value.visit(callback);
     }
 }
 
 impl Visitor for ast::WithItem {
-    fn visit(&mut self, location: &location::Location) {
-        self.context_expr.visit(location);
-        self.optional_vars.visit(location);
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
+        self.context_expr.visit(callback);
+        self.optional_vars.visit(callback);
     }
 }
 
 impl Visitor for ast::ExceptHandler {
-    fn visit(&mut self, location: &location::Location) {
-        self.typ.visit(location);
-        self.body.visit(location);
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
+        self.typ.visit(callback);
+        self.body.visit(callback);
     }
 }
 
 impl Visitor for ast::Varargs {
-    fn visit(&mut self, location: &location::Location) {
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
         match self {
             ast::Varargs::None => (),
             ast::Varargs::Unnamed => (),
-            ast::Varargs::Named(paramater) => paramater.visit(location),
+            ast::Varargs::Named(paramater) => paramater.visit(callback),
         }
     }
 }
 
 impl Visitor for ast::Parameter {
-    fn visit(&mut self, location: &location::Location) {
-        self.annotation.visit(location);
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
+        self.annotation.visit(callback);
     }
 }
 
 impl Visitor for ast::Parameters {
-    fn visit(&mut self, location: &location::Location) {
-        self.args.visit(location);
-        self.kwonlyargs.visit(location);
-        self.vararg.visit(location);
-        self.kwarg.visit(location);
-        self.defaults.visit(location);
-        self.kw_defaults.visit(location);
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
+        self.args.visit(callback);
+        self.kwonlyargs.visit(callback);
+        self.vararg.visit(callback);
+        self.kwarg.visit(callback);
+        self.defaults.visit(callback);
+        self.kw_defaults.visit(callback);
     }
 }
 
 impl Visitor for ast::Expression {
-    fn visit(&mut self, location: &location::Location) {
-        if &mut self.location == location {
-            println!("whoopie!");
-            match &mut self.node {
-                ast::ExpressionType::Binop {a, op, b} => *op = ast::Operator::Mult,
-
-                _ => (),
-            }
-        }
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
+        callback(self);
 
         // recurse
         match &mut self.node {
-            ast::ExpressionType::BoolOp {op: _, values} => values.visit(location),
-            ast::ExpressionType::Binop {a, op: _, b} => { a.visit(location); b.visit(location); },
-            ast::ExpressionType::Subscript {a, b} => { a.visit(location); b.visit(location); },
-            ast::ExpressionType::Unop {op: _, a} => a.visit(location),
-            ast::ExpressionType::Await {value} => value.visit(location),
-            ast::ExpressionType::Yield {value} => value.visit(location),
-            ast::ExpressionType::YieldFrom {value} => value.visit(location),
-            ast::ExpressionType::Compare {vals, ops: _} => vals.visit(location),
-            ast::ExpressionType::Attribute {value, name: _} => value.visit(location),
-            ast::ExpressionType::Call {function, args, keywords} => { function.visit(location); args.visit(location); keywords.visit(location); },
+            ast::ExpressionType::BoolOp {op: _, values} => values.visit(callback),
+            ast::ExpressionType::Binop {a, op: _, b} => { a.visit(callback); b.visit(callback); },
+            ast::ExpressionType::Subscript {a, b} => { a.visit(callback); b.visit(callback); },
+            ast::ExpressionType::Unop {op: _, a} => a.visit(callback),
+            ast::ExpressionType::Await {value} => value.visit(callback),
+            ast::ExpressionType::Yield {value} => value.visit(callback),
+            ast::ExpressionType::YieldFrom {value} => value.visit(callback),
+            ast::ExpressionType::Compare {vals, ops: _} => vals.visit(callback),
+            ast::ExpressionType::Attribute {value, name: _} => value.visit(callback),
+            ast::ExpressionType::Call {function, args, keywords} => { function.visit(callback); args.visit(callback); keywords.visit(callback); },
             ast::ExpressionType::Number {value: _} => (),
-            ast::ExpressionType::List {elements} => elements.visit(location),
-            ast::ExpressionType::Tuple {elements} => elements.visit(location),
-            ast::ExpressionType::Dict {elements} => elements.visit(location),
-            ast::ExpressionType::Set {elements} => elements.visit(location),
-            ast::ExpressionType::Comprehension {kind, generators} => { kind.visit(location); generators.visit(location); },
-            ast::ExpressionType::Starred {value} => value.visit(location),
-            ast::ExpressionType::Slice {elements} => elements.visit(location),
+            ast::ExpressionType::List {elements} => elements.visit(callback),
+            ast::ExpressionType::Tuple {elements} => elements.visit(callback),
+            ast::ExpressionType::Dict {elements} => elements.visit(callback),
+            ast::ExpressionType::Set {elements} => elements.visit(callback),
+            ast::ExpressionType::Comprehension {kind, generators} => { kind.visit(callback); generators.visit(callback); },
+            ast::ExpressionType::Starred {value} => value.visit(callback),
+            ast::ExpressionType::Slice {elements} => elements.visit(callback),
             ast::ExpressionType::String {value: _} => (),
             ast::ExpressionType::Bytes {value: _} => (),
             ast::ExpressionType::Identifier {name: _} => (),
-            ast::ExpressionType::Lambda {args, body} => { args.visit(location); body.visit(location); },
-            ast::ExpressionType::IfExpression {test, body, orelse} => { test.visit(location); body.visit(location); orelse.visit(location); },
+            ast::ExpressionType::Lambda {args, body} => { args.visit(callback); body.visit(callback); },
+            ast::ExpressionType::IfExpression {test, body, orelse} => { test.visit(callback); body.visit(callback); orelse.visit(callback); },
             ast::ExpressionType::True {} => (),
             ast::ExpressionType::False {} => (),
             ast::ExpressionType::None {} => (),
@@ -152,30 +145,30 @@ impl Visitor for ast::Expression {
 }
 
 impl Visitor for ast::Statement {
-    fn visit(&mut self, location: &location::Location) {
+    fn visit(&mut self, callback: &dyn Fn(&mut ast::Located<ast::ExpressionType>)) {
         match &mut self.node {
             ast::StatementType::Break => (),
             ast::StatementType::Continue => (),
-            ast::StatementType::Return {value} => value.visit(location),
+            ast::StatementType::Return {value} => value.visit(callback),
             ast::StatementType::Import {names: _} => (),
             ast::StatementType::ImportFrom {level: _, module: _, names: _} => (),
             ast::StatementType::Pass => (),
-            ast::StatementType::Assert {test, msg} => { test.visit(location); msg.visit(location); },
-            ast::StatementType::Delete {targets} => targets.visit(location),
-            ast::StatementType::Assign {targets, value} => { targets.visit(location); value.visit(location); },
-            ast::StatementType::AugAssign {target, op: _, value} => { target.visit(location); value.visit(location); },
-            ast::StatementType::AnnAssign {target, annotation, value} => { target.visit(location); annotation.visit(location); value.visit(location); },
-            ast::StatementType::Expression {expression} => expression.visit(location),
+            ast::StatementType::Assert {test, msg} => { test.visit(callback); msg.visit(callback); },
+            ast::StatementType::Delete {targets} => targets.visit(callback),
+            ast::StatementType::Assign {targets, value} => { targets.visit(callback); value.visit(callback); },
+            ast::StatementType::AugAssign {target, op: _, value} => { target.visit(callback); value.visit(callback); },
+            ast::StatementType::AnnAssign {target, annotation, value} => { target.visit(callback); annotation.visit(callback); value.visit(callback); },
+            ast::StatementType::Expression {expression} => expression.visit(callback),
             ast::StatementType::Global {names: _} => (),
             ast::StatementType::Nonlocal {names: _} => (),
-            ast::StatementType::If {test, body, orelse} => { test.visit(location); body.visit(location); orelse.visit(location); },
-            ast::StatementType::While {test, body, orelse} => { test.visit(location); body.visit(location); orelse.visit(location); },
-            ast::StatementType::With {is_async: _, items, body} => { items.visit(location); body.visit(location); },
-            ast::StatementType::For {is_async: _, target, iter, body, orelse} => { target.visit(location); iter.visit(location); body.visit(location); orelse.visit(location); },
-            ast::StatementType::Raise {exception, cause} => { exception.visit(location); cause.visit(location); },
-            ast::StatementType::Try {body, handlers, orelse, finalbody} => { body.visit(location); handlers.visit(location); orelse.visit(location); finalbody.visit(location); },
-            ast::StatementType::ClassDef {name: _, body, bases, keywords, decorator_list} => { body.visit(location); bases.visit(location); keywords.visit(location); decorator_list.visit(location); },
-            ast::StatementType::FunctionDef {is_async: _, name: _, args, body, decorator_list, returns} => { args.visit(location); body.visit(location); decorator_list.visit(location); returns.visit(location); },
+            ast::StatementType::If {test, body, orelse} => { test.visit(callback); body.visit(callback); orelse.visit(callback); },
+            ast::StatementType::While {test, body, orelse} => { test.visit(callback); body.visit(callback); orelse.visit(callback); },
+            ast::StatementType::With {is_async: _, items, body} => { items.visit(callback); body.visit(callback); },
+            ast::StatementType::For {is_async: _, target, iter, body, orelse} => { target.visit(callback); iter.visit(callback); body.visit(callback); orelse.visit(callback); },
+            ast::StatementType::Raise {exception, cause} => { exception.visit(callback); cause.visit(callback); },
+            ast::StatementType::Try {body, handlers, orelse, finalbody} => { body.visit(callback); handlers.visit(callback); orelse.visit(callback); finalbody.visit(callback); },
+            ast::StatementType::ClassDef {name: _, body, bases, keywords, decorator_list} => { body.visit(callback); bases.visit(callback); keywords.visit(callback); decorator_list.visit(callback); },
+            ast::StatementType::FunctionDef {is_async: _, name: _, args, body, decorator_list, returns} => { args.visit(callback); body.visit(callback); decorator_list.visit(callback); returns.visit(callback); },
         }
     }
 }
