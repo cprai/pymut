eefe0ffd7377b4d57487208a280be0f3f94a5aac
Chuck Rai <cprai@sfu.ca>
2019-12-07 15:06:32 -0800

    Testing python code

---
+++ math.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def power(a, b):
+    return a ** b
+
+def increment(a):
+    return a + 1
+
+def decrement(a):
+    return a - 1
--- test.py
+++ test.py
@@ -1,24 +1,3 @@
-#i = 0
-#break
-#while i < 3:
-#    x + 17 +2
-#    y
-#    break
-#    continue
-#    if x < 1:
-#        pass
-#    else:
-#        continue
-#    print("hello from python")
-#    i += 1
+import math
 
-#x and not (y and z) or t or False
-#(x + 3.14159) ** 15
-#i + 3 + xor
-
-i = 0
-x = 1
-while i < 14:
-    i = i + 1
-    x = x**2
-
+print(math.add(3, 4))
