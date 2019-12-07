ebcbf405aa32bf8fffeb55e416cdab111da78e1c
Chuck Rai <cprai@sfu.ca>
2019-12-06 22:07:05 -0800

    Refactor running code

--- src/main.rs
+++ src/main.rs
@@ -19,18 +19,20 @@ mod util;
 mod serde_compatibility;
 use crate::mutation::{Mutation, explore_mutations, apply_mutation};
 
-fn run(ast: ast::Program) {
+fn run(ast: ast::Program) -> i64 {
+    let settings = PySettings::default();
+    let vm = VirtualMachine::new(settings);
+    let context = pyobject::PyContext::default();
+    let scope = vm.new_scope_with_builtins();
+
     match compile::compile_program(ast, "".to_string(), 0) {
         Ok(code_object) => {
-            let settings = PySettings::default();
-            let vm = VirtualMachine::new(settings);
-            let scope = vm.new_scope_with_builtins();
-            //let code = obj::objcode::PyCode::new(code_object);
-            //let codeRef = PyRef{code};
-            let context = pyobject::PyContext::default();
-            vm.run_code_obj(context.new_code_object(code_object), scope);
+            match vm.run_code_obj(context.new_code_object(code_object), scope) {
+                Ok(..) => 0,
+                Err(..) => -1,
+            }
         },
-        Err(error) => unreachable!(),
+        Err(..) => -1,
     }
 }
 
@@ -42,12 +44,12 @@ fn main() {
     let mutations: Vec<Mutation> = explore_mutations(&mut program);
 
     for mutation in mutations {
-        let serialized = serde_json::to_string(&mutation).unwrap();
-        println!("serialized = {}", serialized);
+        //let serialized = serde_json::to_string(&mutation).unwrap();
+        //println!("serialized = {}", serialized);
 
         let mut mutated_program = program.clone();
         apply_mutation(&mut mutated_program, mutation);
 
-        run(mutated_program);
+        println!("{} {}", mutated_program == program, run(mutated_program));
     }
 }
