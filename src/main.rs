96683d8292a74bb76151b18d757658225193a2f7
Chuck Rai <cprai@sfu.ca>
2019-12-08 14:43:17 -0800

    Checkpoint save - all I needed wat init_importlib

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,13 @@ use rustpython_vm::{
     pyobject,
     //pyobject::{ItemProtocol, PyResult, PyRef},
     //scope::Scope,
+    import, match_class,
     PySettings,
     VirtualMachine,
 };
 
 mod traversal;
 mod mutation;
-mod util;
 mod serde_compatibility;
 use crate::mutation::{Mutation, explore_mutations, apply_mutation};
 
@@ -57,24 +57,24 @@ enum RunResult {
     Timeout,
 }
 
-fn run(ast: ast::Program) -> RunResult {
-    let settings = PySettings::default();
-    let vm = VirtualMachine::new_with_callback(settings, &|ast, src| {
-        return ast;
-    });
-    let context = pyobject::PyContext::default();
-    let scope = vm.new_scope_with_builtins();
-
-    match compile::compile_program(ast, "".to_string(), 0) {
-        Ok(code_object) => {
-            match vm.run_code_obj(context.new_code_object(code_object), scope) {
-                Ok(..) => RunResult::Sucess,
-                Err(..) => RunResult::RuntimeError,
-            }
-        },
-        Err(..) => RunResult::CompileError,
-    }
-}
+//fn run(ast: ast::Program) -> RunResult {
+//    let settings = PySettings::default();
+//    let vm = VirtualMachine::new_with_callback(settings, &|ast, src| {
+//        return ast;
+//    });
+//    let context = pyobject::PyContext::default();
+//    let scope = vm.new_scope_with_builtins();
+//
+//    match compile::compile_program(ast, "".to_string(), 0) {
+//        Ok(code_object) => {
+//            match vm.run_code_obj(context.new_code_object(code_object), scope) {
+//                Ok(..) => RunResult::Sucess,
+//                Err(..) => RunResult::RuntimeError,
+//            }
+//        },
+//        Err(..) => RunResult::CompileError,
+//    }
+//}
 
 mod schema {
     table! {
@@ -94,17 +94,6 @@ struct MutationEntry {
     file_sha1: String,
     location: i32,
     mutation: String,
-}
-
-struct Helper {
-    callback: Box<dyn Fn(ast::Program, &str) -> ast::Program>,
-}
-
-fn helper(filename: &str, callback: Box<dyn Fn(ast::Program, &str) -> ast::Program>) {
-    let h = Helper{callback: callback};
-    let file = fs::read_to_string(filename).expect("");
-    let program: ast::Program = parser::parse_program(&file).unwrap();
-    (*h.callback)(program, &file);
 }
 
 fn execute(command_line_options: CommandLineOptions) {
@@ -129,32 +118,53 @@ fn execute(command_line_options: CommandLineOptions) {
 
     for mutation_entry in mutation_entries {
 
-        helper(&command_line_options.file, Box::new(move |ast: ast::Program, src: &str| -> ast::Program {
+        let callback = Box::new(move |ast: ast::Program, src: &str| -> ast::Program {
             let target_file_hash = mutation_entry.file_sha1.clone();
-            println!("{}", mutation_entry.mutation);
+            let file_hash = hex::encode(Sha1::digest(src.as_bytes()).as_slice());
+            println!("{}", file_hash);
+
+            if file_hash == target_file_hash {
+                println!("{}", src);
+            }
+
             return ast;
-        }));
+        });
 
-        //let callback = |ast: ast::Program, src: &str| -> ast::Program {
-        //    let newast = ast.clone();
-        //    let target_file_hash = mutation_entry.file_sha1.clone();
-        //    let file_hash = hex::encode(Sha1::digest(src.as_bytes()).as_slice());
-        //    if 2==3 {
-        //        return newast;
-        //    }
+        //let vm = VirtualMachine::new_with_callback(PySettings::default(), callback);
+        //let vm = VirtualMachine::new(PySettings::default());
+        let vm: VirtualMachine = Default::default();
+        let context = pyobject::PyContext::default();
+        let scope = vm.new_scope_with_builtins();
+        import::init_importlib(&vm, cfg!(not(target_os = "wasi")));
 
-        //    return newast;
+        let r = run_script(&vm, scope, &command_line_options.file);
+        println!("{}", r.is_err());
+
+        //let result = match compile::compile(&file, compile::Mode::Exec, command_line_options.file.clone(), vm.settings.optimize) {
+        //    Ok(code_object) => {
+        //        match vm.run_code_obj(context.new_code_object(code_object), scope) {
+        //            Ok(..) => RunResult::Sucess,
+        //            Err(..) => RunResult::RuntimeError,
+        //        }
+        //    },
+        //    Err(..) => RunResult::CompileError,
         //};
 
-        //unsafe {
-        //    let vm = VirtualMachine::new_with_callback(PySettings::default(), &callback);
-        //}
+        /*
 
+        let result = match vm.compile(&file, compile::Mode::Exec, command_line_options.file.clone()) {
+            Ok(code_object) => {
+                vm.
+                match vm.run_code_obj(context.new_code_object((*code_object).code.clone() ), scope) {
+                    Ok(..) => RunResult::Sucess,
+                    Err(..) => RunResult::RuntimeError,
+                }
+            },
+            Err(..) => RunResult::CompileError,
+        };
+
+        */
     }
-
-    //let vm = VirtualMachine::new_with_callback(PySettings::default(), &|ast, src| {
-    //    return ast;
-    //});
 }
 
 fn explore(command_line_options: CommandLineOptions) {
@@ -208,3 +218,68 @@ fn main() {
 //            RunResult::Sucess => println!("-- Uncaught mutation!"),
 //            _ => println!("Caught mutation"),
 //        }
+
+use std::env;
+use std::path::PathBuf;
+use std::process;
+use rustpython_vm::{
+    util,
+    obj::{objint::PyInt, objtuple::PyTuple, objtype},
+    print_exception,
+    pyobject::{ItemProtocol, PyResult},
+    scope::Scope,
+};
+
+fn _run_string(vm: &VirtualMachine, scope: Scope, source: &str, source_path: String) -> PyResult {
+    let code_obj = vm
+        .compile(source, compile::Mode::Exec, source_path.clone())
+        .map_err(|err| vm.new_syntax_error(&err))?;
+    // trace!("Code object: {:?}", code_obj.borrow());
+    scope
+        .globals
+        .set_item("__file__", vm.new_str(source_path), vm)?;
+    vm.run_code_obj(code_obj, scope)
+}
+
+fn run_command(vm: &VirtualMachine, scope: Scope, source: String) -> PyResult<()> {
+    _run_string(vm, scope, &source, "<stdin>".to_string())?;
+    Ok(())
+}
+
+fn run_module(vm: &VirtualMachine, module: &str) -> PyResult<()> {
+    let runpy = vm.import("runpy", &[], 0)?;
+    let run_module_as_main = vm.get_attribute(runpy, "_run_module_as_main")?;
+    vm.invoke(&run_module_as_main, vec![vm.new_str(module.to_owned())])?;
+    Ok(())
+}
+
+fn run_script(vm: &VirtualMachine, scope: Scope, script_file: &str) -> PyResult<()> {
+    // Parse an ast from it:
+    let file_path = PathBuf::from(script_file);
+    let file_path = if file_path.is_file() {
+        file_path
+    } else if file_path.is_dir() {
+        let main_file_path = file_path.join("__main__.py");
+        if main_file_path.is_file() {
+            main_file_path
+        } else {
+            process::exit(1);
+        }
+    } else {
+        process::exit(1);
+    };
+
+    let dir = file_path.parent().unwrap().to_str().unwrap().to_string();
+    let sys_path = vm.get_attribute(vm.sys_module.clone(), "path").unwrap();
+    vm.call_method(&sys_path, "insert", vec![vm.new_int(0), vm.new_str(dir)])?;
+
+    match util::read_file(&file_path) {
+        Ok(source) => {
+            _run_string(vm, scope, &source, file_path.to_str().unwrap().to_string())?;
+        }
+        Err(err) => {
+            process::exit(1);
+        }
+    }
+    Ok(())
+}
