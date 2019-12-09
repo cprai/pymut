ee69f9b61a596ea68076fd9d25063d89211004af
Chuck Rai <cprai@sfu.ca>
2019-12-08 21:49:09 -0800

    Test runner script

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
 
+cargo build
+export PYMUT_PATH=$(readlink -e ./target/debug/pymut)
+
+starting_directory=$(pwd)
+tests_directory=./tests
+
+for test in $tests_directory/*
+do
+    cd $test
+    echo "Running $test"
+    ./run.sh
+    cd $starting_directory
+done
