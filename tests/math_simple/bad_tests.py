4d04ac39c808d11fc6c6032d3dc8c2f697bb4d14
Chuck Rai <cprai@sfu.ca>
2019-12-08 21:09:54 -0800

    Good and bad tests

---
+++ tests/math_simple/bad_tests.py
@@ -0,0 +1,4 @@
+import mymath
+
+assert mymath.add(3, 4) == 7
+assert mymath.subtract(3, 4) == -1
--- tests/math_simple/run.sh
+++ tests/math_simple/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 $PYMUT_PATH -m Explore -d mutations.db -f mymath.py
-$PYMUT_PATH -m Execute -d mutations.db -f test.py
+$PYMUT_PATH -m Execute -d mutations.db -f good_tests.py
+$PYMUT_PATH -m Execute -d mutations.db -f bad_tests.py
 sqlite3 -column mutations.db "select * from results;"
